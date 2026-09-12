302f217db168adafeca5d40a225898c766827803
ebikakpou <ebikakpou@gmail.com>
2026-09-12 08:46:34 +0100

    added other services output to the output.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,3 @@
-
-
 output "instance_id" {
   description = "The provisioned EC2 instance's ID"
   value       = aws_instance.cloudshift_store.id
@@ -16,6 +14,25 @@ output "instance_public_dns" {
 }
 
 output "app_url" {
-  description = "Open this in a browser once the instance finishes booting (~1-2 minutes)"
+  description = "URL to access the CloudShift Store application"
   value       = "http://${aws_instance.cloudshift_store.public_ip}"
+}
+
+output "prometheus_url" {
+  description = "URL to access the Prometheus monitoring dashboard"
+  value       = "http://${aws_instance.cloudshift_store.public_ip}:9090"
+}
+
+output "grafana_url" {
+  description = "URL to access the Grafana monitoring dashboard"
+  value       = "http://${aws_instance.cloudshift_store.public_ip}:3000"
+}
+
+output "docker_services" {
+  description = "URLs for all Docker Compose services automatically started on the EC2 instance"
+  value = {
+    cloudshift_store = "http://${aws_instance.cloudshift_store.public_ip}:80"
+    prometheus       = "http://${aws_instance.cloudshift_store.public_ip}:9090"
+    grafana          = "http://${aws_instance.cloudshift_store.public_ip}:3000"
+  }
 }
